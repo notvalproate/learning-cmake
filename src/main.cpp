d085cfa72e4feb2337e97f47d6f0aa081bacb341
Ian Tauro <sushruth127@gmail.com>
2024-09-28 03:36:25 +0530

    find package working

--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,28 @@
 #include "cprint.hpp"
 #include "Cat.hpp"
 
+#include "zlib.h"
+
 int main() {
     cprint::StringPrinter cprintPrinter(std::cout);
     cprintPrinter.print("Hello, World!");
 
     Cat myCat("Luna");
     myCat.meow();
+
+    const char* data = "Hello, World!";
+    uLong srcLen = strlen(data);
+    uLong destLen = compressBound(srcLen);
+
+    Bytef* compressed = new Bytef[destLen];
+
+    if (compress(compressed, &destLen, (const Bytef*)data, srcLen) == Z_OK) {
+        std::cout << "Data compressed successfully.\n";
+    } else {
+        std::cerr << "Compression failed.\n";
+    }
+
+    delete compressed;
 
     return 0;
 }
